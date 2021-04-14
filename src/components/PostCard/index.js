3d1464413a26615274e9d9ba6e476dcb0ed2d96b
Sebastian Villegas <sebasvil20@gmail.com>
2021-04-14 10:29:01 -0500

    Adding kind of lazy loading using intersection observer and its polyfill

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { Container, TextContainer, Button, Description, Title, AutorDate } from './styles'
+import { useNearScreen } from '../../hooks/useNearScreen'
+import {
+  Container,
+  TextContainer,
+  Button,
+  Description,
+  Title,
+  AutorDate,
+} from './styles'
 
-export const PostCard = ({ id, title, description, cover, userDate }) => {
+export const PostCard = ({ id, title, description, userDate }) => {
+  const [show, element] = useNearScreen()
+
   return (
-    <Container>
-      <TextContainer>
-        <Title>{title}</Title>
-        <AutorDate> {userDate} </AutorDate>
-        <Description>{description}</Description>
-        <Button to={`/detail/${id}`}>VIEW FULL POST</Button>
-      </TextContainer>
+    <Container ref={element}>
+      {show  && ( 
+        <TextContainer>
+          <Title>{title}</Title>
+          <AutorDate> {userDate} </AutorDate>
+          <Description>{description}</Description>
+          <Button to={`/detail/${id}`}>VIEW FULL POST</Button>
+        </TextContainer>
+      )}
     </Container>
   )
 }
